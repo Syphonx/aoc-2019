8947b269c5572b7770576dfecff343ac26685432
Jak Barnes <contact@jakbarnes.co.uk>
2019-12-13 13:18:46 +0000

    Added debug logging and debug input to day 5

--- src/day5.rs
+++ src/day5.rs
@@ -7,7 +7,11 @@ use std::convert::TryInto;
 #[allow(unused_imports)]
 use std::io::{self, BufRead};
 
+const ENABLE_USER_INPUT: bool = false;
+const DEBUG_LOG: bool = false;
 const NUM_REG: usize = 4;
+
+static mut DEBUG_INPUT: i32 = 1;
 static mut REGISTERS: [u32; NUM_REG] = [0; NUM_REG];
 
 struct Digits {
@@ -42,12 +46,16 @@ impl Iterator for Digits {
 
 pub fn mem_set(memory: &mut Vec<i32>, index: i32, value: i32) {
 	let address = memory[index as usize];
-	println!("mem_set({}) = {}", index, value);
+	if DEBUG_LOG {
+		println!("mem_set({}) = {}", index, value);
+	}
 	memory[address as usize] = value;
 }
 
 pub fn mem_set_addr(memory: &mut Vec<i32>, address: i32, value: i32) {
-	println!("mem_set_addr({}) = {}", address, value);
+	if DEBUG_LOG {
+		println!("mem_set_addr({}) = {}", address, value);
+	}
 	memory[address as usize] = value;
 }
 
@@ -68,13 +76,17 @@ pub fn mem_get(memory: &mut Vec<i32>, index: i32, param: usize) -> i32 {
 
 pub fn mem_get_pos(memory: &mut Vec<i32>, address: i32) -> i32 {
 	let value = memory[address as usize];
-	println!("mem_get_pos({}) |0| = {}", address, memory[value as usize]);
+	if DEBUG_LOG {
+		println!("mem_get_pos({}) |0| = {}", address, memory[value as usize]);
+	}
 	return memory[value as usize];
 }
 
 pub fn mem_get_addr(memory: &mut Vec<i32>, address: i32) -> i32 {
 	let value = memory[address as usize];
-	println!("mem_get_addr({}) |1| = {}", address, value);
+	if DEBUG_LOG {
+		println!("mem_get_addr({}) |1| = {}", address, value);
+	}
 	return value;
 }
 
@@ -113,44 +125,58 @@ pub fn reg_clear() {
 }
 
 pub fn opcode_add(memory: &mut Vec<i32>, pc: i32) -> i32 {
-	println!(
-		"opcode_add {}+{}={}",
-		mem_get_noprint(memory, pc, 0),
-		mem_get_noprint(memory, pc + 1, 1),
-		mem_get_noprint(memory, pc, 0) + mem_get_noprint(memory, pc + 1, 1)
-	);
+	if DEBUG_LOG {
+		println!(
+			"opcode_add {}+{}={}",
+			mem_get_noprint(memory, pc, 0),
+			mem_get_noprint(memory, pc + 1, 1),
+			mem_get_noprint(memory, pc, 0) + mem_get_noprint(memory, pc + 1, 1)
+		);
+	}
 	let value = mem_get(memory, pc, 0) + mem_get(memory, pc + 1, 1);
 	mem_set(memory, pc + 2, value);
 	return 4;
 }
 
 pub fn opcode_mul(memory: &mut Vec<i32>, pc: i32) -> i32 {
-	println!(
-		"opcode_mul {}*{}={}",
-		mem_get_noprint(memory, pc, 0),
-		mem_get_noprint(memory, pc + 1, 1),
-		mem_get_noprint(memory, pc, 0) * mem_get_noprint(memory, pc + 1, 1)
-	);
+	if DEBUG_LOG {
+		println!(
+			"opcode_mul {}*{}={}",
+			mem_get_noprint(memory, pc, 0),
+			mem_get_noprint(memory, pc + 1, 1),
+			mem_get_noprint(memory, pc, 0) * mem_get_noprint(memory, pc + 1, 1)
+		);
+	}
 	let value = mem_get(memory, pc, 0) * mem_get(memory, pc + 1, 1);
 	mem_set(memory, pc + 2, value);
 	return 4;
 }
 
 pub fn opcode_in(memory: &mut Vec<i32>, pc: i32) -> i32 {
-	// println!("opcode_in... waiting for input");
-	let stdin = io::stdin();
-	let line1 = stdin
-		.lock()
-		.lines()
-		.next()
-		.unwrap()
-		.unwrap()
-		.parse::<i32>()
-		.expect("Expected integer");
-	// let line1 = 1;
+	if DEBUG_LOG {
+		println!("opcode_in... waiting for input");
+	}
+	let mut input = 0;
+	if ENABLE_USER_INPUT {
+		let stdin = io::stdin();
+		input = stdin
+			.lock()
+			.lines()
+			.next()
+			.unwrap()
+			.unwrap()
+			.parse::<i32>()
+			.expect("Expected integer");
+	} else {
+		unsafe {
+			input = DEBUG_INPUT;
+		}
+	}
 	let value = mem_get_addr(memory, pc);
-	mem_set_addr(memory, value, line1);
-	// println!("opcode_in {} = {}", value, line1);
+	mem_set_addr(memory, value, input);
+	if DEBUG_LOG {
+		println!("opcode_in {} = {}", value, input);
+	}
 	return 2;
 }
 
@@ -160,12 +186,14 @@ pub fn opcode_out(memory: &mut Vec<i32>, pc: i32) -> i32 {
 }
 
 pub fn opcode_jnz(memory: &mut Vec<i32>, pc: i32) -> i32 {
-	println!(
-		"opcode_jnz ({} != 0, {}) = {}",
-		mem_get_noprint(memory, pc, 0),
-		mem_get_noprint(memory, pc, 0) != 0,
-		mem_get_noprint(memory, pc + 1, 1),
-	);
+	if DEBUG_LOG {
+		println!(
+			"opcode_jnz ({} != 0, {}) = {}",
+			mem_get_noprint(memory, pc, 0),
+			mem_get_noprint(memory, pc, 0) != 0,
+			mem_get_noprint(memory, pc + 1, 1),
+		);
+	}
 	if mem_get(memory, pc, 0) != 0 {
 		return mem_get(memory, pc + 1, 1);
 	} else {
@@ -174,12 +202,14 @@ pub fn opcode_jnz(memory: &mut Vec<i32>, pc: i32) -> i32 {
 }
 
 pub fn opcode_jiz(memory: &mut Vec<i32>, pc: i32) -> i32 {
-	println!(
-		"opcode_jiz ({} == 0, {}) = {}",
-		mem_get_noprint(memory, pc, 0),
-		mem_get_noprint(memory, pc, 0) == 0,
-		mem_get_noprint(memory, pc + 1, 1),
-	);
+	if DEBUG_LOG {
+		println!(
+			"opcode_jiz ({} == 0, {}) = {}",
+			mem_get_noprint(memory, pc, 0),
+			mem_get_noprint(memory, pc, 0) == 0,
+			mem_get_noprint(memory, pc + 1, 1),
+		);
+	}
 	if mem_get(memory, pc, 0) == 0 {
 		return mem_get(memory, pc + 1, 1);
 	} else {
@@ -188,13 +218,15 @@ pub fn opcode_jiz(memory: &mut Vec<i32>, pc: i32) -> i32 {
 }
 
 pub fn opcode_lt(memory: &mut Vec<i32>, pc: i32) -> i32 {
-	println!(
-		"opcode_lt({}) ({} < {}, {})",
-		mem_get_noprint(memory, pc + 2, 2),
-		mem_get_noprint(memory, pc, 0),
-		mem_get_noprint(memory, pc + 1, 1),
-		mem_get_noprint(memory, pc, 0) < mem_get_noprint(memory, pc + 1, 1),
-	);
+	if DEBUG_LOG {
+		println!(
+			"opcode_lt({}) ({} < {}, {})",
+			mem_get_noprint(memory, pc + 2, 2),
+			mem_get_noprint(memory, pc, 0),
+			mem_get_noprint(memory, pc + 1, 1),
+			mem_get_noprint(memory, pc, 0) < mem_get_noprint(memory, pc + 1, 1),
+		);
+	}
 	let addr = mem_get_addr(memory, pc + 2);
 	if mem_get(memory, pc, 0) < mem_get(memory, pc + 1, 1) {
 		mem_set_addr(memory, addr, 1);
@@ -205,13 +237,15 @@ pub fn opcode_lt(memory: &mut Vec<i32>, pc: i32) -> i32 {
 }
 
 pub fn opcode_eq(memory: &mut Vec<i32>, pc: i32) -> i32 {
-	println!(
-		"opcode_eq({}) ({} == {}, {})",
-		mem_get_noprint(memory, pc + 2, 2),
-		mem_get_noprint(memory, pc, 0),
-		mem_get_noprint(memory, pc + 1, 1),
-		mem_get_noprint(memory, pc, 0) == mem_get_noprint(memory, pc + 1, 1),
-	);
+	if DEBUG_LOG {
+		println!(
+			"opcode_eq({}) ({} == {}, {})",
+			mem_get_noprint(memory, pc + 2, 2),
+			mem_get_noprint(memory, pc, 0),
+			mem_get_noprint(memory, pc + 1, 1),
+			mem_get_noprint(memory, pc, 0) == mem_get_noprint(memory, pc + 1, 1),
+		);
+	}
 	let addr = mem_get_addr(memory, pc + 2);
 	if mem_get(memory, pc, 0) == mem_get(memory, pc + 1, 1) {
 		mem_set_addr(memory, addr, 1);
@@ -224,9 +258,13 @@ pub fn opcode_eq(memory: &mut Vec<i32>, pc: i32) -> i32 {
 pub fn opcode_to_str(input: i32) -> &'static str {
 	match input {
 		1 => "Add",
-		2 => "Mul",
-		3 => "In",
-		4 => "Out",
+		2 => "Multiply",
+		3 => "Input",
+		4 => "Output",
+		5 => "Jump != zero",
+		6 => "Jump == zero",
+		7 => "Less than",
+		8 => "Equals",
 		99 => "Halt",
 		_ => "____",
 	}
@@ -235,20 +273,30 @@ pub fn opcode_to_str(input: i32) -> &'static str {
 pub fn parse_opcode(instruction: i32) -> i32 {
 	let mut opcode = instruction;
 	let mut digits: Vec<_> = Digits::new(instruction as usize).collect();
-	print!("parse_opcode: '{}' = ", instruction);
+	if DEBUG_LOG {
+		print!("parse_opcode: '{}' = ", instruction);
+	}
 	if digits.len() > 1 {
 		digits.reverse();
 		opcode = (*digits.get(0).expect("Expected i32") as i32)
 			+ ((*digits.get(1).expect("Expected i32") as i32) * 10);
-		print!("{} ({}) ", opcode, opcode_to_str(opcode));
+		if DEBUG_LOG {
+			print!("{} ({}) ", opcode, opcode_to_str(opcode));
+		}
 		for i in 2..digits.len() {
 			reg_set(i - 2, digits[i].try_into().unwrap());
-			print!("{}, ", digits[i]);
+			if DEBUG_LOG {
+				print!("{}, ", digits[i]);
+			}
 		}
-		println!();
+		if DEBUG_LOG {
+			println!();
+		}
 	} else {
-		print!("{} ({}) ", opcode, opcode_to_str(opcode));
-		println!();
+		if DEBUG_LOG {
+			print!("{} ({}) ", opcode, opcode_to_str(opcode));
+			println!();
+		}
 	}
 	return opcode;
 }
@@ -335,27 +383,21 @@ pub fn solve_part1(input: &str) -> i32 {
 		.map(|x| x.trim().parse::<i32>().expect("Expected integer"))
 		.collect();
 	// print_memory(&memory, 0);
+	unsafe {
+		DEBUG_INPUT = 1;
+	}
 	return run_intcode(&mut memory);
 }
 
 #[aoc(day5, part2)]
 pub fn solve_part2(input: &str) -> i32 {
-	// let input = "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9";
-	// let input = "3,3,1105,-1,9,1101,0,0,12,4,12,99,1";
-	// let input = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99";
 	let mut memory: Vec<i32> = input
 		.split_terminator(",")
 		.map(|x| x.trim().parse::<i32>().expect("Expected integer"))
 		.collect();
 	// print_memory(&memory, 0);
-	return run_intcode(&mut memory);
-}
-
-#[cfg(test)]
-mod tests {
-	use super::*;
-	#[test]
-	fn part_1() {
-		assert_eq!(solve_part1("1,9,10,3,2,3,11,0,99,30,40,50"), 3500);
+	unsafe {
+		DEBUG_INPUT = 5;
 	}
+	return run_intcode(&mut memory);
 }
